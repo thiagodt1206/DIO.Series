3b61dd259720b08e611ecaeb3fc0ea6c9695a78c
thiagodt1206 <thi.hobbit@gmail.com>
2021-06-25 14:00:19 -0300

    Primeiro commit do cadastro de series

---
+++ Enum/Genero.cs
@@ -0,0 +1,21 @@
+namespace DIO.Series
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Espionagens = 6,
+        Faroeste = 7,
+        Fantaisa = 8,
+        Ficcao_Cientifica = 9,
+        Musical = 10,
+        Romance = 11,
+        Suspense = 12,
+        terror = 13
+
+
+    }
+}
